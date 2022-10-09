a449655f96a6f06d6975f05790f085cf0cb9a935
NitinPasoriah <nitin.pasoriah@gmail.com>
2022-10-10 00:39:58 +0530

    nav check2

---
+++ flashtype/src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+    render() {
+        return (
+        <div className="app">
+            {/* Nav Section */}
+            <h1>This is my Navigation bar</h1>
+            {/* Landing Page */}
+
+            {/* Challenge Section */}
+
+            {/* Footer */}
+        </div>
+        );
+    }
+}
+export default App;
